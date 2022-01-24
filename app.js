da737972fe1f8f8e87a8aeb9a3080b6d1ac2128c
John Doe <leehy0782@naver.com>
2022-01-24 23:41:21 +0900

    add preventDefault

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const loginForm = document.getElementById('login-form');
 const loginInput = loginForm.querySelector('input');
 const loginButton = loginForm.querySelector('button');
 
-function onLoginSubmit() {
-    const username = loginInput.value;
-    if (username  === '') {
-        alert('Please write your name');
-    }  else if (username.length > 15) {
-        alert('Your name is too long');
-    }
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
 }
 
 loginForm.addEventListener('submit', onLoginSubmit); 
