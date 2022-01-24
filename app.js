633f746cb8d0dd1770bbe50acf862c6795b58ef5
John Doe <leehy0782@naver.com>
2022-01-24 23:21:29 +0900

    add validation to loginInput

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const loginInput = loginForm.querySelector('input');
 const loginButton = loginForm.querySelector('button');
 
 function clickLoginBtn() {
-    console.log('Hello', loginInput.value); 
+    const username = loginInput.value;
+    if (username  === '') {
+        alert('Please write your name');
+    }  else if (username.length > 15) {
+        alert('Your name is too long');
+    }
 }
 
 loginButton.addEventListener('click', clickLoginBtn);
