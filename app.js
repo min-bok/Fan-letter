a440e265e421a92542675fd53e03b195b3f02986
John Doe <leehy0782@naver.com>
2022-01-24 23:14:35 +0900

    connect login input and login btn on javascript

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('hi');
+const loginForm = document.getElementById('login-form');
+const loginInput = loginForm.querySelector('input');
+const loginButton = loginForm.querySelector('button');
+
+function clickLoginBtn() {
+    console.log('Hello', loginInput.value); 
+}
+
+loginButton.addEventListener('click', clickLoginBtn);
