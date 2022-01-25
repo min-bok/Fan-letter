63b366907af6e62369f5ae56f01af554b4aadd7a
John Doe <leehy0782@naver.com>
2022-01-25 15:56:51 +0900

    update validation for username with html

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
-const loginButton = document.querySelector('#login-form button');
 
-function onLonginBtnClick() {
+function onLonginSubmit() {
     const username = loginInput.value;
-    if(username === '') {
-        alert('please write your name');
-    } else if (username.length > 15) {
-        alert('your name is too long')
-    }
+    console.log(username);
 }
 
-loginButton.addEventListener('click', onLonginBtnClick);
+loginForm.addEventListener('submit', onLonginSubmit);
