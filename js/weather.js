41d4436e7d945d97e9a1a0833e185d2472187c19
John Doe <leehy0782@naver.com>
2022-01-26 16:29:31 +0900

    display temp and location

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,14 @@ const API_KEY = '58df8615efe73067fbead169c0274ab8';
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${parseInt(data.main.temp)}`});
 }
 
 function onGeoError() {
