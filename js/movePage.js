62b93c307dba2b80d9896021e166f510ef546951
John Doe <leehy0782@naver.com>
2022-01-26 21:39:56 +0900

    add movepage.js

---
+++ js/movePage.js
@@ -0,0 +1,14 @@
+const body = document.querySelector('body');
+const mainPage = document.querySelector('#mainPage');
+const navBtn = document.querySelector('#navBtn'); 
+const testBtn = document.querySelector('#navBtn button:nth-child(3)'); 
+const testPage = document.querySelector('#testPage');
+
+testPage.remove();
+
+function moveTestPage() {
+    mainPage.remove();
+    body.appendChild(testPage);
+}
+
+testBtn.addEventListener('click', moveTestPage);
