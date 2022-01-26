3467c0e659623ef939c3cd5afaa16668a860dbcf
John Doe <leehy0782@naver.com>
2022-01-26 15:38:22 +0900

    modify overlapping function name

--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,8 @@ function onGeoOk(position) {
     console.log('you live it', lat, lng);
 }
 
-function onGeoOk() {
+function onGeoError() {
     alert(`Can't find you, No weather for you.`);
-
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoOk)
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
