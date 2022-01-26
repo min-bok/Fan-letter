150ffa3496412ca1f44ab4cc22cdb942908fd1a0
John Doe <leehy0782@naver.com>
2022-01-26 14:12:46 +0900

    create newTodoObj and add id that connected to li

--- js/todo.js
+++ js/todo.js
@@ -17,8 +17,9 @@ function deleteTodo(event) {
 
 function paintTodo(newTodo) {
     const li = document.createElement('li');
+    li.id = newTodo.id;
     const span = document.createElement('span');
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement('button');
     button.innerText = '❌';
     button.addEventListener('click', deleteTodo);
@@ -31,8 +32,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = '';
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now()
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
