1f5612271ed1425d176000f97d30d4fc02f9b9a2
John Doe <leehy0782@naver.com>
2022-01-26 15:51:21 +0900

    add api

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,10 @@
+const API_KEY = '58df8615efe73067fbead169c0274ab8';
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log('you live it', lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    console.log(url);
 }
 
 function onGeoError() {
