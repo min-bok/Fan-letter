833f67be3b74013c730c7f30dd8a1e31124c5119
John Doe <leehy0782@naver.com>
2022-01-27 02:28:03 +0900

    test page

--- js/testPage.js
+++ js/testPage.js
@@ -1,3 +1,13 @@
+const question = document.createElement('h1');
+const firstAnswer = document.createElement('button');
+firstAnswer.classList.add('first');
+const secondAnswer = document.createElement('button');
+secondAnswer.classList.add('second');
+
+testPage.appendChild(question);
+testPage.appendChild(firstAnswer);
+testPage.appendChild(secondAnswer);
+
 const qnaArr = [
     {
         question: `'철인회'라는 단체가 생긴다는데 참여해볼까?`,
@@ -5,7 +15,6 @@ const qnaArr = [
                     무슨 주의니 뭐니 나는 글만 쓰는 사람인데`,
         answerTwo: `고독은 문학을 낳고, 사교는 인격을 도야한다..
                     뜻이 맞는 사람끼리 모여 같이 활동을 하면 좋겠군!`,
-        id: parseInt(Date.now())
     },
     {
         question: `새로운 급사가 왔다. 문학도라는데, 사정이 딱하다.
@@ -14,20 +23,17 @@ const qnaArr = [
                     내가 한 번 봐줄께.`,
         answerTwo: `마침 급사도 필요하던 참인데,
                     지용이 형 원고 좀 받아올래?`,
-        id: parseInt(Date.now())
     },
     {
         question: `교정을 봐달라는 부탁을 받았는데,
                    어라? 이건 비문 같은데?`,
         answerOne: `이건 비문이다. 라고 사실대로 이야기 한다.`,
         answerTwo: `자존심이 상하지는 않을까? 내가 이야기 해도 되나?`,
-        id: parseInt(Date.now())
     },
     {
         question: `이번에 나온 글이 반응이 영 그래.`,
         answerOne: `이건 비문이다. 라고 사실대로 이야기 한다.`,
         answerTwo: `자존심이 상하지는 않을까? 내가 이야기 해도 되나?`,
-        id: Date.now()
     },
     {
         question: `내 뮤즈에게서 답장이 오지를 않고 있다.`,
@@ -36,7 +42,6 @@ const qnaArr = [
         answerTwo: `내 글에 문제가 있었나.
                     내가 잘못해서 마음이 상한 것은 아닌가.
                     지금 즈음이면 답이 와야 하는데.`,
-        id: Date.now()
     },
     {
         question: `작품 완성이 목전인데, 몸 상태가 심상치 않다.
@@ -45,20 +50,19 @@ const qnaArr = [
                     나에게는 이 작품 뿐이다.`,
         answerTwo: `살아있어야 쓰지.
                     내 몸 망쳐가면서 쓰는 작품이 정말 의미가 있나? 회복에 전념한다.`,
-        id: Date.now()
     },
 ];
 
-function loadLayout() {
-    const question = document.createElement('h1');
-    const firstAnswer = document.createElement('button');
-    firstAnswer.classList.add('first');
-    const secondAnswer = document.createElement('button');
-    secondAnswer.classList.add('second');
+// function loadLayout() {
+//     const question = document.createElement('h1');
+//     const firstAnswer = document.createElement('button');
+//     firstAnswer.classList.add('first');
+//     const secondAnswer = document.createElement('button');
+//     secondAnswer.classList.add('second');
 
-    testPage.appendChild(question);
-    testPage.appendChild(firstAnswer);
-    testPage.appendChild(secondAnswer);
-}
+//     testPage.appendChild(question);
+//     testPage.appendChild(firstAnswer);
+//     testPage.appendChild(secondAnswer);
+// }
 
-window.addEventListener('load', loadLayout);
+// window.addEventListener('load', loadLayout);
