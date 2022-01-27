8948713a5ed298f197f6a631b7a9d7e865b929e3
John Doe <leehy0782@naver.com>
2022-01-27 22:21:45 +0900

    add synopsis page

--- js/movePage.js
+++ js/movePage.js
@@ -3,14 +3,24 @@ const mainPage = document.querySelector('#mainPage');
 const navBtn = document.querySelector('#navBtn'); 
 const testBtn = document.querySelector('#navBtn button:nth-child(3)'); 
 const test = document.querySelector('#test');
+const synopsis = document.querySelector('#synopsis');
+const CharacterBtn = document.querySelector('#navBtn button:nth-child(1)'); 
+const synopsisBtn = document.querySelector('#navBtn button:nth-child(2)'); 
 
 test.remove();
+synopsis.remove();
 
 function moveTestPage() {
     mainPage.remove();
     body.prepend(test);
 }
 
+function moveSynopsisPage() {
+    mainPage.remove();
+    body.prepend(synopsis);
+}
+
+synopsisBtn.addEventListener('click', moveSynopsisPage)
 testBtn.addEventListener('click', moveTestPage);
 
 
