6e430757ec4d79525667a23a5a186cdd7e5865ea
John Doe <leehy0782@naver.com>
2022-02-04 23:49:59 +0900

    add hover function on charater card in charcterInfo.js

--- js/CharacterInfo.js
+++ js/CharacterInfo.js
@@ -1,16 +1,25 @@
 function paintCharacter() {
     for (let i =0; i < resultArr.length; i++) {
         const characterWrap = document.createElement('div');
+        characterWrap.classList.add('container');
+        const frontCard = document.createElement('div');
+        frontCard.classList.add('front');
+        frontCard.classList.add('card');
+        const backCard = document.createElement('div');
+        backCard.classList.add('back');
+        backCard.classList.add('card');
         const face = document.createElement('img');
         const name = document.createElement('h1');
         const age = document.createElement('p');
         const who = document.createElement('p');
     
         characterInfo.appendChild(characterWrap);
-        characterWrap.appendChild(face);
-        characterWrap.appendChild(name);
-        characterWrap.appendChild(age);
-        characterWrap.appendChild(who);
+        characterWrap.appendChild(frontCard);
+        characterWrap.appendChild(backCard);
+        frontCard.appendChild(face);
+        frontCard.appendChild(name);
+        frontCard.appendChild(age);
+        backCard.appendChild(who);
 
         face.src = `../img/${i}.png`
         name.innerText = resultArr[i].name;
