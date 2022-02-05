43db0cfca128829bc52002bcffb709392a002d76
John Doe <leehy0782@naver.com>
2022-02-05 14:24:24 +0900

    update result page img

--- js/test.js
+++ js/test.js
@@ -39,7 +39,7 @@ function resultOne() {
         i = i-9;
     }
 
-    CharacterImg.src = `../img/${i}.png`;
+    CharacterImg.src = `../img/P${i}.png`;
     CharacterName.innerText = resultArr[i].name;
     CharacterInfo.innerText = resultArr[i].intro;
     CharacterDetail.innerText = resultArr[i].detail;
