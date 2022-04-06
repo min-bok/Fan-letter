89e5c1b0d23cb3615387f933d540b64834c67196
John Doe <leehy0782@naver.com>
2022-04-06 18:14:01 +0900

    create index_bundles.js

--- js/index.js
+++ js/index.js
@@ -2,5 +2,16 @@ import greeting from "./greeting";
 import clock from "./clock";
 import quotes from "./quotes";
 
+import todo from "./todo";
+import weather from "./weather";
+import movePage from "./movePage";
+import testInfo from "./testInfo";
+import resultInfo from "./resultInfo";
+import test from "./test";
+import synopsisquotes from "./synopsis";
+import CharacterInfo from "./CharacterInfo";
+import navBtn from "./navBtn";
+import jquerySakura from "./jquery-sakura";
+
 // import css from "../css/style.css";
 // import sakura from "../css/jquery-sakura.css";
