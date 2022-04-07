c81f169341a63bb728793a9908804528231c1973
John Doe <leehy0782@naver.com>
2022-04-07 22:19:21 +0900

    update mainPage.js

--- js/mainPage.js
+++ js/mainPage.js
@@ -87,3 +87,40 @@ if (savedToDos !== null) {
     toDos = parsedToDos;
     parsedToDos.forEach(paintTodo);
 }
+
+// main page - clock
+const clock = document.querySelector('#clock');
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+// main page - weather
+
+const API_KEY = '58df8615efe73067fbead169c0274ab8';
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector('#weather span:first-child');
+        const city = document.querySelector('#weather span:last-child');
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${parseInt(data.main.temp)}°C`});
+}
+
+function onGeoError() {
+    alert(`현재 위치를 찾을 수 없습니다😢`);
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
