2895fe9fb8d5dfcccbc129892e0fbfcdad7991c2
John Doe <leehy0782@naver.com>
2022-04-09 22:58:48 +0900

    add skura effect at main page and synopsis page

--- source/index.js
+++ source/index.js
@@ -3,7 +3,8 @@ import todo from "../js/todo";
 import clock from "../js/clock";
 import quotes from "../js/quotes";
 import weather from "../js/weather";
+import sakura from "../js/jquery-sakura"
 
+import sakuraStyle from "../css/jquery-sakura.css";
 import backgroundStyle from "../css/background.css";
 import mainPage from "../css/mainPage.css"
-// import sakura from "../css/jquery-sakura.css";
--- source/synopsisPage.js
+++ source/synopsisPage.js
@@ -1,4 +1,6 @@
 import synopsis from "../js/synopsis";
+import sakura from "../js/jquery-sakura"
 
+import sakuraStyle from "../css/jquery-sakura.css";
 import backgroundStyle from "../css/background.css";
 import synopsisStyle from "../css/synopsis.css"
