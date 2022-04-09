9b2c98bd6e73753cebddd15c3bc8523fdf816df0
John Doe <leehy0782@naver.com>
2022-04-09 23:11:49 +0900

    remove sakura effect

--- source/index.js
+++ source/index.js
@@ -3,9 +3,9 @@ import todo from "../js/todo";
 import clock from "../js/clock";
 import quotes from "../js/quotes";
 import weather from "../js/weather";
-import sakura from "../js/jquery-sakura"
+// import sakura from "../js/jquery-sakura"
 // import movePage from "../js/movePage"
 
-import sakuraStyle from "../css/jquery-sakura.css";
+// import sakuraStyle from "../css/jquery-sakura.css";
 import backgroundStyle from "../css/background.css";
 import mainPage from "../css/mainPage.css"
--- source/synopsisPage.js
+++ source/synopsisPage.js
@@ -1,6 +1,6 @@
 import synopsis from "../js/synopsis";
-import sakura from "../js/jquery-sakura"
+// import sakura from "../js/jquery-sakura"
 
-import sakuraStyle from "../css/jquery-sakura.css";
+// import sakuraStyle from "../css/jquery-sakura.css";
 import backgroundStyle from "../css/background.css";
 import synopsisStyle from "../css/synopsis.css"
