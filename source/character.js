177f088d19d48a94e12412aaa3c1c5c91b951805
John Doe <leehy0782@naver.com>
2022-04-13 23:15:33 +0900

    update character page paintCharacter function

--- source/character.js
+++ source/character.js
@@ -1,5 +1,5 @@
+import info from "../js/resultInfo"
 import character from "../js/CharacterInfo.js";
-import info from "../js/resultInfo.js";
 
 import backgroundStyle from "../css/background.css";
 import characterStyle from "../css/character.css"
